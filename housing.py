a6c1b9e9e6f90374fec5f4365959e0f71ef65b06
ketankishore27 <41575470+ketankishore27@users.noreply.github.com>
2018-11-07 19:23:31 +0530

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  6 08:22:32 2018
-
-@author: kekishor
-"""
-
 # ------------------------> Importing the required Libraries -----------------------------------
 
 import pandas as pd
@@ -27,7 +20,44 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = np.nan, strategy = 'median')
 imputer.fit(dataset['LotFrontage'].values.reshape(-1, 1))
 dataset['LotFrontage'] = imputer.transform(dataset['LotFrontage'].values.reshape(-1, 1))
-dataset[dataset['MasVnrType'].isnull()].drop()
+
+# ------> Dropping some columns with nan values ---
+dataset.drop(['PoolQC', 'Fence', 'MiscFeature'], axis = 1, inplace = True)
+dataset.drop('FireplaceQu', axis = 1, inplace=True)
+sns.heatmap(dataset.isnull(), cbar=False, cmap='viridis')
+
+dataset.loc[dataset['MasVnrType'].isnull(), 'MasVnrType'] = 'None'
+dataset.loc[dataset['MasVnrArea'].isnull(), 'MasVnrArea'] = 0
+dataset.dropna(axis = 0, inplace = True)
+
+#dataset.drop('Utilities', axis = 1, inplace = True)
+#dataset.drop('Street', axis=1, inplace = True)
+#len(dataset[dataset['LandContour'] == 'Lvl'])
+#dataset.drop('LandContour', axis = 1, inplace = True)
+dataset_filter = dataset
+
+from sklearn.preprocessing import LabelEncoder
+for i in (dataset.columns):
+    if type(dataset[i][1]) == str:
+        encoder = LabelEncoder()
+        dataset[i] = encoder.fit_transform(dataset[i])
+
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+        
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 1000, random_state = 0)
+regressor.fit(X_train, y_train)
+
+from sklearn.model_selection import cross_val_score
+score = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
+
+score.mean()
+
+
 
 
 
