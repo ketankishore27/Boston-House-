c862132adcd9d3bd7bf3b8376eb30ff197637f1c
ketankishore27 <41575470+ketankishore27@users.noreply.github.com>
2018-11-08 19:35:33 +0530

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,3 +1,4 @@
+
 # ------------------------> Importing the required Libraries -----------------------------------
 
 import pandas as pd
@@ -9,74 +10,72 @@ from copy import deepcopy
 # ------------------------> Getting the data ---------------------------------------------------
 
 dataset = pd.read_csv('train.csv')
-
+dataset1 = pd.read_csv('test.csv')
 # ------------------------> Preprocessing the data ---------------------------------------------
-
-dataset.drop('Alley', axis = 1, inplace = True)
-dataset.drop('Id', axis=1, inplace = True)
-
-# - (Checking and evaluating null values) - 
-
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = np.nan, strategy = 'median')
-imputer.fit(dataset['LotFrontage'].values.reshape(-1, 1))
-dataset['LotFrontage'] = imputer.transform(dataset['LotFrontage'].values.reshape(-1, 1))
-
-# ------> Dropping some columns with nan values ---
-
-dataset.drop(['PoolQC', 'Fence', 'MiscFeature'], axis = 1, inplace = True)
-dataset.drop('FireplaceQu', axis = 1, inplace=True)
-sns.heatmap(dataset.isnull(), cbar=False, cmap='viridis')
-
-# -------> Substitute suitable Values
-
-dataset.loc[dataset['MasVnrType'].isnull(), 'MasVnrType'] = 'None'
-dataset.loc[dataset['MasVnrArea'].isnull(), 'MasVnrArea'] = 0
-
-# -------.. Dropping of nan values
-
-dataset.dropna(axis = 0, inplace = True)
-
-#dataset.drop('Utilities', axis = 1, inplace = True)
-#dataset.drop('Street', axis=1, inplace = True)
-#len(dataset[dataset['LandContour'] == 'Lvl'])
-#dataset.drop('LandContour', axis = 1, inplace = True)
-
-# ------> Taking backup
-
-dataset_filter = deepcopy(dataset)
 
 column = []
 from sklearn.preprocessing import LabelEncoder
-for i in (dataset.columns):
+for i in dataset.columns:
     if type(dataset[i][1]) == str:
-        encoder = LabelEncoder()
-        dataset[i] = encoder.fit_transform(dataset[i])
+#        encoder = LabelEncoder()
+#        dataset[i] = encoder.fit_transform(dataset[i])
         column.append(i)
-        
-# -----------------> Getting dummy values -------------------
 
 X = dataset.iloc[:, :-1]
+X = dataset
 y = dataset.iloc[:, -1]
 
-X = pd.get_dummies(X, columns = column, drop_first = True)
-        
-# -----------------> Splitting the data ---------------------
+X = pd.get_dummies(X, drop_first = True)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.preprocessing import Imputer
+for i in X.columns:
+    if X[i].isnull().any():
+        imputer = Imputer(missing_values = np.nan, strategy = 'mean')
+        imputer.fit(X[i].values.reshape(-1, 1))
+        X[i] = imputer.transform(X[i].values.reshape(-1, 1))
+
+# -----------------> Splitting the data ---------------------
+        
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # -----------------> Applying Random Forest ----------------
 
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 1000, random_state = 0)
-regressor.fit(X_train, y_train)
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+regressor.fit(X, y)
 
 # -----------------> Using Cross val Score -----------------------
 
 from sklearn.model_selection import cross_val_score
-score = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
+score = cross_val_score(estimator = regressor, X = X, y = y, cv = 10)
 
 score.mean()
 
-y_pred = regressor.predict(X_test)
+#feature = []
+#for i in range(len(regressor.feature_importances_)):
+#    feature.append([i, regressor.feature_importances_[i]])
+#    
+#feature.sort(key = lambda x: x[1], reverse = True)
+#
+##X = X.iloc[:, [i[0] for i in feature[0: 16: 1]]]
+#
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+#
+#from sklearn.ensemble import RandomForestRegressor
+#regressor = RandomForestRegressor(n_estimators = 100, random_state = 0, n_jobs=-1)
+#regressor.fit(X_train, y_train)
+#
+#from sklearn.model_selection import cross_val_score
+#score = cross_val_score(estimator = regressor, X = X_train, y = y_train, cv = 10)
+#
+#score.mean()
+
+y_pred = regressor.predict(X.iloc[:, :-1])
+
+
+
+    
+    
+
